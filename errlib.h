c25faa8d727dc3d993cb838f24f1b6d935f7c61a
Matthew T Geiger <matthewgeiger99@gmail.com>
2018-12-29 16:38:43 -0800

    Added errlib.h

---
+++ errlib.h
@@ -0,0 +1,125 @@
+#ifndef __ERR_LIB__
+#define __ERR_LIB__
+
+#include "errlibvar.h"
+
+// No flush
+void NORETURN
+_err_exit(const char *fmt, __STD_ARGS)
+{
+    va_list argList;
+
+    fprintf(stderr, "Error: ");
+
+    va_start(argList, fmt);
+    vfprintf(stderr, fmt, argList);
+    va_end(argList);
+
+    _exit(EXIT_FAILURE);
+}
+
+static void NORETURN
+terminate(Boolean useExit3)
+{
+    char *s;
+
+    /* Dump core if EN_DUMPCORE environment variable is defined and
+    is a nonempty string; otherwise call exit(3) or _exit(2),
+    depending on the value of 'useExit3'. */
+
+    s = getenv("EF_DUMPCORE");
+
+    if (s != NULL && *s != '\0')
+        abort();
+    else if(useExit3)
+        exit(EXIT_FAILURE);
+    else
+        _exit(EXIT_FAILURE);
+}
+
+void NORETURN
+errnoExit(const char *fmt, __STD_ARGS)
+{
+    va_list argList;
+
+    fflush(stdout);
+
+    fprintf(stderr, "errno: %s | ", strerror(errno));
+
+    va_start(argList, fmt);
+    vfprintf(stderr, fmt, argList);
+    va_end(argList);
+
+    fflush(stderr);
+
+    exit(EXIT_FAILURE);
+}
+
+
+void NORETURN
+usageErr(const char *format, __STD_ARGS) {
+    va_list argList;
+
+    // wait for stdout
+    fflush(stdout);
+
+    fprintf(stderr, "Usage: ");
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
+    fflush(stderr);
+    exit(EXIT_FAILURE);
+}
+
+void NORETURN
+errExit(const char *format, __STD_ARGS)
+{
+    va_list argList;
+
+    // wait for stdout
+    fflush(stdout);
+
+    fprintf(stderr, "Error: ");
+    va_start(argList, format);
+    vfprintf(stderr, format, argList);
+    va_end(argList);
+
+    fflush(stderr);
+    exit(EXIT_FAILURE);
+}
+
+void NORETURN
+fatal(const char *fmt)
+{
+    // wait for stdout
+    fflush(stdout);
+
+    fprintf(stderr, "%s\n", fmt);
+    
+    fflush(stderr);
+    exit(EXIT_FAILURE);
+}
+
+static void NORETURN
+outputError(Boolean useErr, Boolean flushStdout,
+        const char *fmt, __STD_ARGS)
+{
+    va_list ap;
+    char userMsg[_BUF_SIZE];
+
+    va_start(ap, fmt);
+    vsnprintf(userMsg, _BUF_SIZE, fmt, ap);
+    va_end(ap);
+
+    if(useErr)
+        errnoExit("%s\n", userMsg);
+    else if(!flushStdout)
+        _err_exit("%s", userMsg);
+    else
+        errExit("%s\n", userMsg);
+
+    terminate(FALSE);
+}
+
+#endif
---
+++ main.c
@@ -0,0 +1,44 @@
+// Linux FD Copy
+// main.c
+
+#include "errlib.h"
+
+int
+main(const int argc, const char *argv[])
+{
+    int inputFd, outputFd, openFlags;
+    mode_t filePerms;
+    ssize_t numRead;
+    char buf[BUF_SIZE];
+
+    if (argc != 3 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s old-file new-file\n", argv[0]);
+
+    /* Open input and output files */
+
+    inputFd = open(argv[1], O_RDONLY);
+    if (inputFd == -1)
+        outputError(TRUE, TRUE, "opening file %s", argv[1]);
+
+    openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+                S_IROTH | S_IWOTH;
+    outputFd = open(argv[2], openFlags, filePerms);
+    if(outputFd == -1)
+        errnoExit("opening file %s\n", argv[2]);
+
+    /* Transfer data until we encounter end of input or error */
+
+    while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+        if(write(outputFd, buf, numRead) != numRead)
+            fatal("couldn't write whole buffer");
+    if(numRead == -1)
+        outputError(TRUE, TRUE, "read");
+
+    if(close(inputFd) == -1)
+        outputError(TRUE, TRUE, "close input");
+    if(close(outputFd) == -1)
+        outputError(TRUE, TRUE, "close output");
+
+    return EXIT_SUCCESS;
+}
